f99b210807a868bae4a862c25cc529c0549f2f09
Mark Thompson <moomoomamoo@gmail.com>
2018-07-22 21:04:07 -0700

    got placeholder for list page up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import * as firebase from "firebase";
 
+import { GlobalService } from '../services/global.service';
+
 @Component({
   selector: 'keto-home',
   templateUrl: './home.component.html',
@@ -11,14 +13,49 @@ export class HomeComponent {
 
   loading: boolean;
 
-  constructor() {
+  Object: any;
+
+  constructor(public globalService: GlobalService) {
   }
 
   ngOnInit() {
+    this.Object = Object;
+
     // this.loading = true;
     console.log("Hello from home");
   	// console.log(firebase);
   }
 
   ngAfterViewInit() {}
+
+  getItemObjsAry() {
+    if (!this.globalService || !this.globalService.dbSnapshot) {
+      return [];
+    }
+
+    var itemObjs = this.globalService.dbSnapshot.itemObjs;
+    var itemObjsAry = [];
+
+    for (var key of Object.keys(itemObjs || {})) {
+        itemObjsAry.push(itemObjs[key]);
+    }
+
+    return itemObjsAry;
+  }
+
+  getArrayOfServings(itemObj) {
+    var servings = [];
+
+    for (var key of Object.keys(itemObj.servings || {})) {
+      servings.push(itemObj.servings[key]);
+    }
+
+    servings.sort((a, b) => {
+      var aName = a.name || "";
+      var bName = b.name || "";
+      return aName.localeCompare(bName);
+    });
+
+    return servings;
+  }
 }
